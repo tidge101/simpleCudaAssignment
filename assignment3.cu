5851fe24e43abe6c1d3bd3c946ff337fd640bda8
Sarah Chong <chong127@mail.chapman.edu>
2018-01-22 10:10:04 -0800

    Program works as expected

--- assignment3.cu
+++ assignment3.cu
@@ -11,9 +11,9 @@
 using namespace std;
 
 // Kernel function that has each thread save its id
-__global__ void save_id(int n, int *dev_tid){
+__global__ void save_id(int n, float *tid){
 	for(int i = 0; i < n; i++){
-		*dev_tid = blockIdx.x;
+		tid[i] = threadIdx.x;
 	}
 }
 
@@ -24,21 +24,24 @@ int main(int argc, char* argv[]){
 	cout << "Number of threads created: " << numThreads << "\n";
 
 	// Create appropriate variables and allocate memory on device
-	int tid;
-	int *dev_tid;
-	HANDLE_ERROR( cudaMalloc( (void**)&dev_tid, sizeof(int) ) );
+	float *tid, *dev_tid;
+	tid = (float*)malloc(numThreads*sizeof(float));
+	HANDLE_ERROR( cudaMalloc(&dev_tid, numThreads*sizeof(float)));
 	// Call kernel function
-	save_id<<<numThreads,1>>>(numThreads, dev_tid);
+	save_id<<<1, numThreads>>>(numThreads, dev_tid);
 
 	// Copy thread id's to host
-	HANDLE_ERROR( cudaMemcpy( &tid, dev_tid, sizeof(int),
+	HANDLE_ERROR( cudaMemcpy( tid, dev_tid, numThreads*sizeof(float),
 														cudaMemcpyDeviceToHost ) );
 
 	// Print thread id's
 	for(int i = 0; i < numThreads; i++){
-		printf("ID of Thread[%d]: %d", i, i.dev_tid);
+		printf("ID of Thread[%d]: %d\n", i, tid[i]);
 	}
 
 	// Free the memory we allocated
 	cudaFree(dev_tid);
+	free(tid);
+
+	return 0;
 }
